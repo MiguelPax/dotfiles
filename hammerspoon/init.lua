f4cb89d3827eb92a7e3f5f54d2e0c2e7790b1dab
MiguelPax <miguelmholton@gmail.com>
2022-01-14 11:53:48 +0100

    added VimMode to hammerspoon

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -2,7 +2,17 @@ inspect = hs.inspect.inspect
 prefix = require("prefix")
 utils = require("utils")
 
-
 require("double_cmdq_to_quit")
 require("keymaps")
 
+
+local VimMode = hs.loadSpoon('VimMode')
+local vim = VimMode:new()
+
+vim:enterWithSequence('jk')
+
+vim
+  :disableForApp('Code')
+  :disableForApp('MacVim')
+  :disableForApp('zoom.us')
+  :enterWithSequence('jk')
